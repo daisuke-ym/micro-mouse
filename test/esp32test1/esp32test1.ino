5c47170939ccfce1fb15b887a98f96db2149b5a9
daisuke <daisuke.ym@gmail.com>
2024-06-18 11:16:43 +0900

    半固定抵抗でモータの回転速度を変えるコードを追加

--- test/esp32test1/esp32test1.ino
+++ test/esp32test1/esp32test1.ino
@@ -5,6 +5,7 @@ BluetoothSerial SerialBT;
 const int LED = 23;
 const int AIN1 = 16;
 const int AIN2 = 17;
+const int VR1 = 26;
 
 const int DELAY = 2000;
 
@@ -17,10 +18,12 @@ void setup() {
 	analogWrite(AIN2, 0);
 	analogWriteFrequency(AIN1, 5e3);
 	analogWriteFrequency(AIN2, 3e3);
+
+	analogReadResolution(12);
 }
 
 void loop() {
-	motor_test();
+	motor_vr_test();
 }
 
 void blink() {
@@ -50,4 +53,19 @@ void motor_test() {
 	}
 	analogWrite(AIN2, 0);
 	delay(DELAY);
+}
+
+void motor_vr_test() {
+	int sensor = analogRead(VR1);
+	int duty = map(sensor, 0, 4095, 0, 255);
+
+	/*
+	SerialBT.print(sensor);
+	SerialBT.print("\t");
+	SerialBT.println(duty);
+	*/
+	analogWrite(AIN1, duty);
+	analogWrite(AIN2, 0);
+
+	delay(100);
 }
