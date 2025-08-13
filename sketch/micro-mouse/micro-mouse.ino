247cad74a0058abd041162d8957a4cb425bcee36
daisuke <daisuke.ym@gmail.com>
2025-08-13 16:18:56 +0900

    壁センサの読み込みを単純移動平均に変更した。

--- sketch/micro-mouse/micro-mouse.ino
+++ sketch/micro-mouse/micro-mouse.ino
@@ -35,7 +35,8 @@ void setup() {
   delay(1000);
 
   // センサの値を読むタスク
-  xTaskCreateUniversal(read_sensor, "task_read_sensor", 8192, NULL, 1, NULL, APP_CPU_NUM);
+  //xTaskCreateUniversal(read_sensor, "task_read_sensor", 8192, NULL, 1, NULL, APP_CPU_NUM);
+  xTaskCreateUniversal(read_sensor_sma, "task_read_sensor_sma", 8192, NULL, 1, NULL, APP_CPU_NUM);
   // センサの値を出力するタスク
   xTaskCreateUniversal(print_sensor, "task_print_sensor", 8192, NULL, 1, NULL, APP_CPU_NUM);
   // ステッパの値を出力するタスク
@@ -88,6 +89,63 @@ void read_sensor(void *pvParameters) {
     // 全部の IRLED を OFF にする
     digitalWrite(IRLED_L_FR, LOW);
     digitalWrite(IRLED_R_FL, LOW);
+    // Task を回すうえで必ず必要な delay
+    delay(1);
+  }
+}
+
+// ----------------------------------------------------------------------
+// センサの値を読み取るタスク(単純移動平均版)
+void read_sensor_sma(void *pvParameters) {
+  int ir_on[2], ir_off[2];
+  int ss_l[10], ss_r[10], ss_fl[10], ss_fr[10];
+  int index = 0;
+
+  while (1) {
+    if (index >= 10) {
+      index = 0;
+    }
+    // 全部の IRLED を OFF にしてセンサの値を読む
+    digitalWrite(IRLED_L_FR, LOW);
+    digitalWrite(IRLED_R_FL, LOW);
+    ir_off[0] = analogRead(SENSOR_R);
+    ir_off[1] = analogRead(SENSOR_FL);
+    // R と FL の IRLED を ON にしてセンサの値を読む
+    digitalWrite(IRLED_R_FL, HIGH);
+    ir_on[0] = analogRead(SENSOR_R);
+    ir_on[1] = analogRead(SENSOR_FL);
+    ss_r[index] = ir_on[0] - ir_off[0];
+    ss_fl[index] = ir_on[1] - ir_off[1];
+    // 全部の IRLED を OFF にしてセンサの値を読む
+    digitalWrite(IRLED_L_FR, LOW);
+    digitalWrite(IRLED_R_FL, LOW);
+    ir_off[0] = analogRead(SENSOR_L);
+    ir_off[1] = analogRead(SENSOR_FR);
+    // L と FR の IRLED を ON にしてセンサの値を読む
+    digitalWrite(IRLED_L_FR, HIGH);
+    ir_on[0] = analogRead(SENSOR_L);
+    ir_on[1] = analogRead(SENSOR_FR);
+    ss_l[index] = ir_on[0] - ir_off[0];
+    ss_fr[index] = ir_on[1] - ir_off[1];
+    // 全部の IRLED を OFF にする
+    digitalWrite(IRLED_L_FR, LOW);
+    digitalWrite(IRLED_R_FL, LOW);
+    // 単純移動平均を求める
+    SS_L = 0;
+    SS_R = 0;
+    SS_FL = 0;
+    SS_FR = 0;
+    for (int i = 0; i < 10; i++) {
+      SS_L += ss_l[i];
+      SS_R += ss_r[i];
+      SS_FL += ss_fl[i];
+      SS_FR += ss_fr[i];
+    }
+    SS_L = SS_L / 10;
+    SS_R = SS_R / 10;
+    SS_FL = SS_FL / 10;
+    SS_FR = SS_FR / 10;
+    index++;
     // Task を回すうえで必ず必要な delay
     delay(1);
   }
